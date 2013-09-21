5dc71df89006d37ad0656d63713e96007ca00e99
Michal Kuklis <michal.kuklis@gmail.com>
2013-09-21 13:29:15 -0400

    export SimpleSocket constructor.

--- index.js
+++ index.js
@@ -1,89 +1,87 @@
-(function () {
+module.exports = SimpleSocket;
 
-  var SimpleSocket = window.SimpleSocket = function (url, options) {
-    options || (options = {});
-    
-    this.url = url;
-    this.protocols = options.protocols;
-    this.reconnectDelay = options.reconnectDelay || 1000;
-    this.closeInterval = options.timeoutInterval || 2000;
-
-    this.readyState = WebSocket.CONNECTING;
-    this.forcedClose = false;
-    this.timedOut = false;
-
-    this.connect();
-  }
-
-  SimpleSocket.prototype.connect = function (reconnect) {
-    var self = this;
-
-    this.socket = new WebSocket(this.url, this.protocols);
-    this.onconnecting && this.onconnecting();
-
-    var closeIntervalId = setTimeout(function () {
-      self.timedOut = true;
-      self.socket.close();
-      self.timedOut = false;
-    }, this.closeInterval);
+var SimpleSocket = function (url, options) {
+  options || (options = {});
   
-    this.socket.onopen = function (event) {
-      clearTimeout(closeIntervalId);
+  this.url = url;
+  this.protocols = options.protocols;
+  this.reconnectDelay = options.reconnectDelay || 1000;
+  this.closeInterval = options.timeoutInterval || 2000;
 
-      self.readyState = WebSocket.OPEN;
-      reconnect = false;
-      self.onopen && self.onopen(event);
-    };
-    
-    this.socket.onclose = function (event) {
-      clearTimeout(closeIntervalId);
-      self.socket = null;
+  this.readyState = WebSocket.CONNECTING;
+  this.forcedClose = false;
+  this.timedOut = false;
 
-      if (self.forcedClose) {
-        self.readyState = WebSocket.CLOSED;
+  this.connect();
+}
+
+SimpleSocket.prototype.connect = function (reconnect) {
+  var self = this;
+
+  this.socket = new WebSocket(this.url, this.protocols);
+  this.onconnecting && this.onconnecting();
+
+  var closeIntervalId = setTimeout(function () {
+    self.timedOut = true;
+    self.socket.close();
+    self.timedOut = false;
+  }, this.closeInterval);
+
+  this.socket.onopen = function (event) {
+    clearTimeout(closeIntervalId);
+
+    self.readyState = WebSocket.OPEN;
+    reconnect = false;
+    self.onopen && self.onopen(event);
+  };
+  
+  this.socket.onclose = function (event) {
+    clearTimeout(closeIntervalId);
+    self.socket = null;
+
+    if (self.forcedClose) {
+      self.readyState = WebSocket.CLOSED;
+      self.onclose && self.onclose(event);
+    } 
+    else {
+      self.readyState = WebSocket.CONNECTING;
+      self.onconnecting && self.onconnecting();
+      
+      if (!reconnect && !self.timedOut) {
         self.onclose && self.onclose(event);
-      } 
-      else {
-        self.readyState = WebSocket.CONNECTING;
-        self.onconnecting && self.onconnecting();
-        
-        if (!reconnect && !self.timedOut) {
-          self.onclose && self.onclose(event);
-        }
-
-        setTimeout(function () {
-          self.connect(true);
-        }, self.reconnectDelay);
       }
-    };
 
-    this.socket.onmessage = function (event) {
-      self.onmessage && self.onmessage(event);
-    };
-
-    this.socket.onerror = function (event) {
-      self.onerror && self.onerror(event);
-    };
-  }
-
-  SimpleSocket.prototype.send = function (data) {
-    if (this.socket) {
-      return this.socket.send(data);
+      setTimeout(function () {
+        self.connect(true);
+      }, self.reconnectDelay);
     }
-  }
+  };
 
-  SimpleSocket.prototype.close = function () {
-    this.forcedClose = true;
-    
-    if (this.socket) {
-      this.socket.close();
-    }
-  }
+  this.socket.onmessage = function (event) {
+    self.onmessage && self.onmessage(event);
+  };
 
-  SimpleSocket.prototype.refresh = function () {
-    if (this.socket) {
-      this.socket.close();
-    }
-  }
+  this.socket.onerror = function (event) {
+    self.onerror && self.onerror(event);
+  };
+}
 
-})();
+SimpleSocket.prototype.send = function (data) {
+  if (this.socket) {
+    return this.socket.send(data);
+  }
+}
+
+SimpleSocket.prototype.close = function () {
+  this.forcedClose = true;
+  
+  if (this.socket) {
+    this.socket.close();
+  }
+}
+
+SimpleSocket.prototype.refresh = function () {
+  if (this.socket) {
+    this.socket.close();
+  }
+}
