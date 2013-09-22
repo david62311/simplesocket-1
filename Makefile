f4f181dda7722520f1a5a06ecedf19f07ad424d4
Michal Kuklis <michal.kuklis@gmail.com>
2013-09-22 19:44:51 -0400

    Added uglify target.

--- Makefile
+++ Makefile
@@ -4,6 +4,11 @@ build: components index.js
 components: component.json
 	@component install --dev
 
+uglify:
+	@component build -s SimpleSocket
+	@uglifyjs -nc --unsafe -mt -o simplesocket.min.js build/build.js
+	@mv build/build.js simplesocket.js
+
 clean:
 	rm -rf build components
 
---
+++ simplesocket.js
@@ -0,0 +1,297 @@
+;(function(){
+
+/**
+ * Require the given path.
+ *
+ * @param {String} path
+ * @return {Object} exports
+ * @api public
+ */
+
+function require(path, parent, orig) {
+  var resolved = require.resolve(path);
+
+  // lookup failed
+  if (null == resolved) {
+    orig = orig || path;
+    parent = parent || 'root';
+    var err = new Error('Failed to require "' + orig + '" from "' + parent + '"');
+    err.path = orig;
+    err.parent = parent;
+    err.require = true;
+    throw err;
+  }
+
+  var module = require.modules[resolved];
+
+  // perform real require()
+  // by invoking the module's
+  // registered function
+  if (!module._resolving && !module.exports) {
+    var mod = {};
+    mod.exports = {};
+    mod.client = mod.component = true;
+    module._resolving = true;
+    module.call(this, mod.exports, require.relative(resolved), mod);
+    delete module._resolving;
+    module.exports = mod.exports;
+  }
+
+  return module.exports;
+}
+
+/**
+ * Registered modules.
+ */
+
+require.modules = {};
+
+/**
+ * Registered aliases.
+ */
+
+require.aliases = {};
+
+/**
+ * Resolve `path`.
+ *
+ * Lookup:
+ *
+ *   - PATH/index.js
+ *   - PATH.js
+ *   - PATH
+ *
+ * @param {String} path
+ * @return {String} path or null
+ * @api private
+ */
+
+require.resolve = function(path) {
+  if (path.charAt(0) === '/') path = path.slice(1);
+
+  var paths = [
+    path,
+    path + '.js',
+    path + '.json',
+    path + '/index.js',
+    path + '/index.json'
+  ];
+
+  for (var i = 0; i < paths.length; i++) {
+    var path = paths[i];
+    if (require.modules.hasOwnProperty(path)) return path;
+    if (require.aliases.hasOwnProperty(path)) return require.aliases[path];
+  }
+};
+
+/**
+ * Normalize `path` relative to the current path.
+ *
+ * @param {String} curr
+ * @param {String} path
+ * @return {String}
+ * @api private
+ */
+
+require.normalize = function(curr, path) {
+  var segs = [];
+
+  if ('.' != path.charAt(0)) return path;
+
+  curr = curr.split('/');
+  path = path.split('/');
+
+  for (var i = 0; i < path.length; ++i) {
+    if ('..' == path[i]) {
+      curr.pop();
+    } else if ('.' != path[i] && '' != path[i]) {
+      segs.push(path[i]);
+    }
+  }
+
+  return curr.concat(segs).join('/');
+};
+
+/**
+ * Register module at `path` with callback `definition`.
+ *
+ * @param {String} path
+ * @param {Function} definition
+ * @api private
+ */
+
+require.register = function(path, definition) {
+  require.modules[path] = definition;
+};
+
+/**
+ * Alias a module definition.
+ *
+ * @param {String} from
+ * @param {String} to
+ * @api private
+ */
+
+require.alias = function(from, to) {
+  if (!require.modules.hasOwnProperty(from)) {
+    throw new Error('Failed to alias "' + from + '", it does not exist');
+  }
+  require.aliases[to] = from;
+};
+
+/**
+ * Return a require function relative to the `parent` path.
+ *
+ * @param {String} parent
+ * @return {Function}
+ * @api private
+ */
+
+require.relative = function(parent) {
+  var p = require.normalize(parent, '..');
+
+  /**
+   * lastIndexOf helper.
+   */
+
+  function lastIndexOf(arr, obj) {
+    var i = arr.length;
+    while (i--) {
+      if (arr[i] === obj) return i;
+    }
+    return -1;
+  }
+
+  /**
+   * The relative require() itself.
+   */
+
+  function localRequire(path) {
+    var resolved = localRequire.resolve(path);
+    return require(resolved, parent, path);
+  }
+
+  /**
+   * Resolve relative to the parent.
+   */
+
+  localRequire.resolve = function(path) {
+    var c = path.charAt(0);
+    if ('/' == c) return path.slice(1);
+    if ('.' == c) return require.normalize(p, path);
+
+    // resolve deps by returning
+    // the dep in the nearest "deps"
+    // directory
+    var segs = parent.split('/');
+    var i = lastIndexOf(segs, 'deps') + 1;
+    if (!i) i = 0;
+    path = segs.slice(0, i + 1).join('/') + '/deps/' + path;
+    return path;
+  };
+
+  /**
+   * Check if module is defined at `path`.
+   */
+
+  localRequire.exists = function(path) {
+    return require.modules.hasOwnProperty(localRequire.resolve(path));
+  };
+
+  return localRequire;
+};
+require.register("simplesocket/index.js", function(exports, require, module){
+module.exports = SimpleSocket;
+
+function SimpleSocket(url, protocols, options) {
+  this.options = options || {};
+  this.url = url;
+  this.protocols = protocols;
+  this.reconnectDelay = this.options.reconnectDelay || 1000;
+  this.closeDelay = this.options.closeDelay || 2000;
+
+  this.readyState = WebSocket.CONNECTING;
+  this.forcedClose = false;
+  this.timedOut = false;
+
+  this.connect();
+}
+
+SimpleSocket.prototype.connect = function (reconnect) {
+  var self = this;
+
+  this.socket = new WebSocket(this.url, this.protocols, this.options);
+  this.onconnecting && this.onconnecting();
+
+  var closeIntervalId = setTimeout(function () {
+    self.timedOut = true;
+    self.socket.close();
+    self.timedOut = false;
+  }, this.closeDelay);
+
+  this.socket.onopen = function (event) {
+    clearTimeout(closeIntervalId);
+
+    self.readyState = WebSocket.OPEN;
+    reconnect = false;
+    self.onopen && self.onopen(event);
+  };
+  
+  this.socket.onclose = function (event) {
+    clearTimeout(closeIntervalId);
+    self.socket = null;
+
+    if (self.forcedClose) {
+      self.readyState = WebSocket.CLOSED;
+      self.onclose && self.onclose(event);
+    } 
+    else {
+      self.readyState = WebSocket.CONNECTING;
+      self.onconnecting && self.onconnecting();
+      
+      if (!reconnect && !self.timedOut) {
+        self.onclose && self.onclose(event);
+      }
+
+      setTimeout(function () {
+        self.connect(true);
+      }, self.reconnectDelay);
+    }
+  };
+
+  this.socket.onmessage = function (event) {
+    self.onmessage && self.onmessage(event);
+  };
+
+  this.socket.onerror = function (event) {
+    self.onerror && self.onerror(event);
+  };
+}
+
+SimpleSocket.prototype.send = function (data) {
+  if (this.socket) {
+    return this.socket.send(data);
+  }
+}
+
+SimpleSocket.prototype.close = function () {
+  this.forcedClose = true;
+  
+  if (this.socket) {
+    this.socket.close();
+  }
+}
+
+SimpleSocket.prototype.refresh = function () {
+  if (this.socket) {
+    this.socket.close();
+  }
+}
+});
+require.alias("simplesocket/index.js", "simplesocket/index.js");if (typeof exports == "object") {
+  module.exports = require("simplesocket");
+} else if (typeof define == "function" && define.amd) {
+  define(function(){ return require("simplesocket"); });
+} else {
+  this["SimpleSocket"] = require("simplesocket");
+}})();
